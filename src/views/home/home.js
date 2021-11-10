94a8380c0c17efd9e0264dd66ba6557fbfcd9280
layerpie <garethdominic112@gmail.com>
2021-11-10 20:36:56 +0700

    Latihan 5.5

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default class Home extends React.Component{
-   render(){
-       return(
-           <div>
-               <h2>Title Home Component</h2>
-               <p>Hello my name is {this.props.name}</p>
-           </div>
-       )
-   } 
-        
-
-
-
+  constructor(props){
+      super(props);
+          this.state={name : this.props.name}
+      }
+      render(){
+          return(
+              <div>
+                  <h2>Title Home Component</h2>
+                  <p>Hello my name is {this.state.name}</p>
+                  <button onClick={()=> this.setState({name : 'Tyrion Lannister'})}> Change my name </button>
+              </div>
+          )
+      }
   }
+
+  
   
